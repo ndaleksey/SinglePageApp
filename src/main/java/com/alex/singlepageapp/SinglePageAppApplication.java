bf19235a9a66d450935c83dbef03039b23eed918
alexeyshishkov <alexey.shishkov@norbit.ru>
2018-05-17 18:18:55 +0300

    Initial commit

---
+++ src/main/java/com/alex/singlepageapp/SinglePageAppApplication.java
@@ -0,0 +1,12 @@
+package com.alex.singlepageapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinglePageAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinglePageAppApplication.class, args);
+	}
+}
